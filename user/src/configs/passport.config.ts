15cb87a4621324b906f52f6bd02e59815048520b
Deepanshu Chaid <bantar117@gmail.com>
2025-12-02 14:52:41 +0000

    passport

--- user/src/configs/passport.config.ts
+++ user/src/configs/passport.config.ts
@@ -4,10 +4,9 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        "74911273408-u367bp0vheh2rgorttvt ov0jk1ajc53r.apps.googleusercontent.com",
-      clientSecret: "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",
-      callbackURL: "http://www.example.com/auth/google/callback",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret : process.env.GOOGLE_SECRET,
+      callbackURL: "http://www.example.com/api/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, cb) {
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
