dafb83c87fd0be6caced03802bd2c1d9b0231275
Deepanshu Chaid <bantar117@gmail.com>
2025-12-06 08:46:21 +0000

    author Schema created

---
+++ author/dist/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import prisma from "./prisma.js";
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+    res.send("hellow world author service");
+});
+app.listen(PORT, async () => {
+    console.log("Author Server is running on port " + PORT);
+    const data = await prisma.user.findMany();
+    console.log(data);
+});
