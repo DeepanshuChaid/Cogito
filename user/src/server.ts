8574da418eff4b69d708b944b10743f243533da8
Deepanshu Chaid <bantar117@gmail.com>
2025-12-06 07:12:36 +0000

    added clouianry configs

--- user/src/server.ts
+++ user/src/server.ts
@@ -8,9 +8,17 @@ import prisma from "./prisma.js";
 import passport from "passport";
 import session from "express-session";
 import { isAuthenticatedMiddleware } from "./middlewares/isAuthenticatedMiddleware.js";
+import {v2 as cloudinary} from 'cloudinary'
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
