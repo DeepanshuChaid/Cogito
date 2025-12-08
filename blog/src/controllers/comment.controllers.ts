82a99d4c4a498fc60fcc164410106f24a23e1413
Deepanshu Chaid <bantar117@gmail.com>
2025-12-08 17:35:20 +0000

    commment delete controontrontrotroroolleer

--- blog/src/controllers/comment.controllers.ts
+++ blog/src/controllers/comment.controllers.ts
@@ -1,64 +1,112 @@
 import { asyncHandler } from "../middlewares/asyncHandler.js";
 import prisma from "../prisma.js";
-import { invalidateCache, invalidateRecommendedBlogsCache } from "../utils/redis.utils.js";
+import {
+  invalidateCache,
+  invalidateRecommendedBlogsCache,
+  getCachedData,
+  setCachedData,
+} from "../utils/redis.utils.js";
 
 export const createCommentController = asyncHandler(async (req, res) => {
   const userId = req.user?.id;
   const blogId = req.params.blogId;
 
-  const { comment, parentId } = req.body
+  const { comment, parentId } = req.body;
 
   const blog = await prisma.blog.findUnique({
     where: {
-      id: blogId
-    }
-  })
+      id: blogId,
+    },
+  });
 
   const newComment = await prisma.comments.create({
     data: {
       comment,
       user: { connect: { id: userId } },
       blog: { connect: { id: blogId } },
-      ...(parentId && { parent: { connect: { id: parentId } } })
-    }
-  })
+      ...(parentId && { parent: { connect: { id: parentId } } }),
+    },
+  });
 
-  if (!newComment) throw new Error("Error creating comment")
-
-  await invalidateCache([
-    `blog:${blogId}`,
-    `user_blogs:${userId}`,
-    'recommended_blogs:all',
-  ])
-
-  await invalidateRecommendedBlogsCache(blog.category);
+  if (!newComment) throw new Error("Error creating comment");
 
   return res.status(201).json({
     message: "Comment created successfully",
-    data: newComment
-  })
-})
-
+    data: newComment,
+  });
+});
 
 // *************************** //
 // GET ALL COMMENTS OF BLOGS CONTROLLER
 // *************************** //
 export const getAllCommentsInBlogController = asyncHandler(async (req, res) => {
-  const blogId = req.params.blogId
+  const blogId = req.params.blogId;
+
+  const cacheKey = `blog_comments:${blogId}`;
+
+  const cache = await getCachedData(
+    res,
+    cacheKey,
+    "Comments fetched successfully",
+  );
+  if (cache) return;
 
   const comments = await prisma.comments.findMany({
     where: {
-      blogId
+      blogId,
     },
     include: {
-      replies: true
-    }
-  })
+      replies: true,
+    },
+  });
 
-  if (!comments) throw new Error("Error getting comments")
+  if (!comments) throw new Error("Error getting comments");
+
+  await setCachedData(cacheKey, comments);
 
   return res.status(200).json({
     message: "Comments fetched successfully",
-    data: comments
+    data: comments,
+  });
+});
+
+
+// *************************** //
+// DELETE COMMENTS IN A BLOGS CONTROLLER
+// *************************** //
+export const deleteCommentController = asynchHandler(async (req, res) => {
+  const blogId = req.params.blogId;
+  const commentId = req.params.id;
+  const userId = req.user?.id
+
+  const blog = await prisma.blog.findUnique({where: {id: blogId}})
+
+  const role = blog.authorId === userId ? "author" : "user"
+
+  const comment = await prisma.comments.findUnique({where: {id: commentId}})
+
+  if (!comment) throw new Error("Comment not found")
+
+  if (comment.userId !== userId && role !== "author") {
+    throw new Error("You are not authorized to delete this comment")
+  }
+
+  if (role === "author") {
+    await prisma.comments.delete({where: {id: commentId}})
+
+    return res.status(200).json({
+      message: "Comment deleted successfully"
+      role,
+      data: comment
+    })
+  }
+
+  await prisma.comments.delete({where: {id: commentId}})
+  
+  return res.status(200).json({
+    message: "Comment deleted successfully"
+    role,
+    data: comment
   })
+  
 })
