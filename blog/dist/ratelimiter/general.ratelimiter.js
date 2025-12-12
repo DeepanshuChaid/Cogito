073456101cad496d4ec5ff84cbc3b1664a4b2761
Deepanshu Chaid <bantar117@gmail.com>
2025-12-12 15:00:58 +0000

    ADDING COMMENT CACHING

--- blog/dist/ratelimiter/general.ratelimiter.js
+++ blog/dist/ratelimiter/general.ratelimiter.js
@@ -1,1 +1,16 @@
-export {};
+import rateLimit from "express-rate-limit";
+import RedisStore from "rate-limit-redis";
+import { redisClient } from "./redis.js";
+// Limit to 20 requests per minute per IP
+export const generalLimiter = rateLimit({
+    store: new RedisStore({
+        sendCommand: (...args) => redisClient.call(...args),
+    }),
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 20, // max 20 requests per window
+    standardHeaders: true, // X-RateLimit headers
+    legacyHeaders: false,
+    message: {
+        message: "Too many requests from this IP. Please try again in a minute.",
+    },
+});
