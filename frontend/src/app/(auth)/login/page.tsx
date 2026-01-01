622aa2bfc79eff2286337cfa9866846c113e0832
Deepanshu Chaid <bantar117@gmail.com>
2026-01-01 17:54:10 +0000

    favicon

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -92,7 +92,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex flex-col items-center gap-6 min-h-svh px-2 py-12 md:p-2">
+    <div className="flex flex-col justify-center items-center gap-6 min-h-svh px-2 py-12 md:p-2">
       <div className="flex w-full flex-col gap-[32px] justify-center items-center">
         <Link href="/" className="flex item-center self-center">
           <Image src="/Logo.png" alt="logo" height={40} width={104} className="h-[40px] w-auto"
