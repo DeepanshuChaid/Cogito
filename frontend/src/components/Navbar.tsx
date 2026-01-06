bfbbe041ca052750dd6c173208de952fcbcdeb84
Deepanshu Chaid <bantar117@gmail.com>
2026-01-06 20:27:28 +0000

    NAVBAR

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -12,20 +12,19 @@ export default function Navbar () {
 
       
 
-      <div className="flex gap-[-12px]">
-        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <div className="flex flex-row w-full max-w-[420px] justify-center items-center">
+        <svg className="mr-[-64px] z-10" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M17 17L13.1422 13.1422M15.2222 8.11111C15.2222 12.0385 12.0385 15.2222 8.11111 15.2222C4.18375 15.2222 1 12.0385 1 8.11111C1 4.18375 4.18375 1 8.11111 1C12.0385 1 15.2222 4.18375 15.2222 8.11111Z" stroke="#CCCCCC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
-        cdcd
+        <input
+           id="email"
+           type="email"
+           placeholder="Search..."
+           className={` mx-auto flex max-w-[420px]
+                  pl-12 pr-5 py-3 bg-[linear-gradient(6.02deg,_#0F0F0F_-27.3%,_#1F1F1F_198.01%)] border border-white/10
+                  rounded-full w-full px-4 bg-[#1A1A1A] border shadow-[0_0_6px_2px_rgba(0,0,0,0.2),inset_0_1px_1px_rgba(255,255,255,0.05)] text-14 text-[#F2F2F2] placeholder-white-400 focus:outline-none focus:border-white-300 focus:ring-1 focus:ring-white-200/20`}
+         />
       </div>
-      <input
-         id="email"
-         type="email"
-         placeholder="Search..."
-         className={` mx-auto flex max-w-[420px] items-center gap-2.5
-                px-5 py-3 bg-[linear-gradient(6.02deg,_#0F0F0F_-27.3%,_#1F1F1F_198.01%)] border border-white/10
-                rounded-full w-full px-4 bg-[#1A1A1A] border shadow-[0_0_6px_2px_rgba(0,0,0,0.2),inset_0_1px_1px_rgba(255,255,255,0.05)] text-14 text-[#F2F2F2] placeholder-white-400 focus:outline-none focus:border-white-300 focus:ring-1 focus:ring-white-200/20`}
-       />
 
 
 
